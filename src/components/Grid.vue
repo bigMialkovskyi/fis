7ce44a9683f864391db2e15b430e91e3215dfea2
Vlad <vladmalpro37@gmail.com>
2023-01-09 17:55:16 +0300

    first commit

---
+++ src/components/Grid.vue
@@ -0,0 +1,156 @@
+<template>
+  <button @click="deselectRows">deselect rows</button>
+  <ag-grid-vue
+    class="ag-theme-alpine"
+    style="height: 500px"
+    :columnDefs="columnDefs.value"
+    :rowData="createRandData()"
+    :defaultColDef="defaultColDef"
+    rowSelection="multiple"
+    animateRows="true"
+    @cell-clicked="cellWasClicked"
+    @grid-ready="onGridReady"
+  >
+  </ag-grid-vue>
+</template>
+
+<script>
+import { AgGridVue } from "ag-grid-vue3"; // the AG Grid Vue Component
+import { reactive, onMounted, ref } from "vue";
+
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+export default {
+  name: "Grid",
+  components: {
+    AgGridVue,
+  },
+
+  setup() {
+    const gridApi = ref(null); // Optional - for accessing Grid's API
+
+    // Obtain API from grid's onGridReady event
+    const onGridReady = (params) => {
+      gridApi.value = params.api;
+    };
+
+    const rowData = this.createRandData(); // Set rowData to Array of Objects, one Object per Row
+    // let rowData = reactive({}); // Set rowData to Array of Objects, one Object per Row
+
+    // Each Column Definition results in one Column.
+    const columnDefs = reactive({
+      value: [
+        // { field: "make" },
+        // { field: "model" },
+        // { field: "price" },
+        { field: "val1" },
+        { field: "val2" },
+        { field: "val3" },
+        { field: "val4" },
+        { field: "val5" },
+        { field: "val6" },
+        { field: "val7" },
+        { field: "val8" },
+      ],
+    });
+
+    // DefaultColDef sets props common to all Columns
+    const defaultColDef = {
+      sortable: true,
+      filter: true,
+      flex: 1,
+    };
+
+    // Example load data from sever
+    onMounted(() => {
+      fetch("https://www.ag-grid.com/example-assets/row-data.json")
+        .then((result) => result.json())
+        .then((remoteRowData) => (rowData.value = this.createRandData()));
+    });
+    // onMounted(() => {
+    //   fetch("https://www.ag-grid.com/example-assets/row-data.json")
+    //     .then((result) => result.json())
+    //     .then((remoteRowData) => (rowData.value = remoteRowData));
+    // });
+    // onMounted(() => {
+    //   createRandData();
+    //   rowData = this.createRandData();
+    // });
+    console.log(rowData);
+    return {
+      onGridReady,
+      columnDefs,
+      rowData,
+      defaultColDef,
+      cellWasClicked: (event) => {
+        // Example of consuming Grid Event
+        console.log("cell was clicked", event);
+      },
+      deselectRows: () => {
+        gridApi.value.deselectAll();
+      },
+    };
+  },
+
+  methods: {
+    createRandData() {
+      function generateRandomString() {
+        const characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let randomString = "";
+        for (let i = 0; i < 10; i++) {
+          randomString += characters.charAt(
+            Math.floor(Math.random() * characters.length)
+          );
+        }
+        return randomString;
+      }
+
+      function generateRandomFloat(min, max, decimal) {
+        return (Math.random() * (max - min) + min).toFixed(decimal);
+      }
+
+      function generateRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
+      function generateRandomImagePath() {
+        const imagePaths = [
+          "images/image1.jpg",
+          "images/image2.jpg",
+          "images/image3.jpg",
+          "images/image4.jpg",
+          "images/image5.jpg",
+        ];
+        const randomIndex = Math.floor(Math.random() * imagePaths.length);
+        return imagePaths[randomIndex];
+      }
+
+      function getRanomStringInArr() {
+        const imagePaths = ["str1", "str2", "str3", "str4", "str5"];
+        const randomIndex = Math.floor(Math.random() * imagePaths.length);
+        return imagePaths[randomIndex];
+      }
+      let randContent = [];
+
+      for (let i = 0; i < 100; i++) {
+        randContent.push({
+          val1: generateRandomString(),
+          val2: Number(generateRandomFloat(1, 100, 2)),
+          val3: Number(generateRandomFloat(1, 100, 4)),
+          val4: generateRandomInt(0, 1000),
+          val5: generateRandomInt(0, 1000),
+          val6: generateRandomImagePath(),
+          val7: generateRandomString(),
+          val8: getRanomStringInArr(),
+        });
+      }
+      console.log(randContent);
+      return randContent;
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Grid/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Grid from '@/components/Grid.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorl
+    Grid
   }
 }
 </script>
